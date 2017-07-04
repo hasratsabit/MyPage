4861844a3c8bc68db7318ac783477ded3a58de80
Hasrat <hsabit111@gmail.com>
2017-07-04 10:30:38 -0400

    Admin navbar basic frame is created

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-	res.render("index", { title: "Welcome to web development"})
+	res.render("index", { title: "Hasrat Sabit"})
 })
 
 module.exports = router;
